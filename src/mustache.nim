977ee669f8bb47254a79f1e1c642b621d36e5025
soasme <soasme@gmail.com>
2019-09-14 23:02:04 +1200

    define asValue for some built-in kinds.

--- src/mustache.nim
+++ src/mustache.nim
@@ -1,7 +1,7 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-import tables
+import tables, sequtils, sugar
 
 import mustachepkg/submodule
 
@@ -68,6 +68,7 @@ type
     delimiter*: Delimiter
     root*: Token
 
+type
   ValueKind* {.pure.}= enum
     vkInt,
     vkFloat,
@@ -77,16 +78,54 @@ type
     vkSeq,
     vkTable
 
-  Value* = ref object
+  Value* = object
     case kind*: ValueKind
     of vkInt: vInt: int
     of vkFloat: vFloat: float
     of vkString: vString: string
     of vkBool: vBool: bool
-    of vkProc: vProc: proc
+    of vkProc: vProc: proc()
     of vkSeq: vSeq: seq[Value]
-    of vkTable: vTable: Table[string, Value]
+    of vkTable: vTable: ref Table[string, Value]
 
+  Context* = ref object
+    values: Table[string, Value]
+
+proc asValue*(value: int): Value =
+  Value(kind: vkInt, vInt: value)
+
+proc asValue*(value: float): Value =
+  Value(kind: vkFloat, vFloat: value)
+
+proc asValue*(value: string): Value =
+  Value(kind: vkString, vString: value)
+
+proc asValue*(value: bool): Value =
+  Value(kind: vkBool, vBool: value)
+
+proc asValue*[T](value: Table[string, T]): Value =
+  let newValue = new(Table[string, Value])
+  for k, v in value.pairs:
+    newValue[k] = v.asValue
+  Value(kind: vkTable, vTable: newValue)
+
+proc asValue*[T](value: seq[T]): Value =
+  Value(kind: vkSeq, vSeq: value.map(asValue))
+
+proc `[]=`*[T](ctx: Context, key: string, value: T) =
+  ## Combine the key with a value within the given ctx.
+  ## Example:
+  ##
+  ##   ctx["name"] = 1
+  ##   ctx["name"] = "string"
+  ##   ctx["name"] = @[1,2,3]
+  ctx.values[key] = value.asValue
 
 when isMainModule:
+  let ctx = Context(values: initTable[string,Value]())
+  ctx["name"] = 1
+  ctx["key"] = "string"
+  ctx["seq"] = @[1,2,3]
+  ctx["seq"] = @["a","b","c"]
+  ctx["table"] = {"a": "b"}.toTable
   echo("hello world")
