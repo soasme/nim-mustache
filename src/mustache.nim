1193ecf81ebbb333f32f5e73fab7a100384ed9b5
soasme <soasme@gmail.com>
2019-09-14 22:36:19 +1200

    add ValueKinds and Value.

--- src/mustache.nim
+++ src/mustache.nim
@@ -1,6 +1,8 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
+import tables
+
 import mustachepkg/submodule
 
 type
@@ -25,20 +27,66 @@ type
     else: discard
 
   Delimiter* = ref object
-    opener: string
-    closer: string
+    opener*: string
+    closer*: string
 
   Token* = ref object of RootObj
+    doc*: string
+    pos*: int
+    size*: int
 
   Text* = ref object of Token
+
   EscapedTag* = ref object of Token
+    name*: string
+    tag*: string
+
   UnescapedTag* = ref object of Token
+    name*: string
+    tag*: string
+
   Section* = ref object of Token
+    name*: string
+    tag*: string
+    inverted*: bool
+    children*: seq[Token]
+    delimiter*: Delimiter
+
   Partial* = ref object of Token
+
   SetDelimiter* = ref object of Token
+    delimiter*: Delimiter
+
+  ParseState* {.pure.} = enum
+    ScanningVerbatim,
+    ScanningTag,
+    OpeningTag,
+    ClosingTag
 
   State* = ref object
-    delimiter: Delimiter
+    state*: ParseState
+    delimiter*: Delimiter
+    root*: Token
+
+  ValueKind* {.pure.}= enum
+    vkInt,
+    vkFloat,
+    vkString,
+    vkBool,
+    vkProc,
+    vkSeq,
+    vkTable
+
+  Value* = ref object
+    case kind*: ValueKind
+    of vkInt: vInt: int
+    of vkFloat: vFloat: float
+    of vkString: vString: string
+    of vkBool: vBool: bool
+    of vkProc: vProc: proc
+    of vkSeq: vSeq: seq[Value]
+    of vkTable: vTable: Table[string, Value]
+
 
 when isMainModule:
   echo("hello world")
