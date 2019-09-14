bc55c07e139079d694af51f046a8868a0e3be78e
soasme <soasme@gmail.com>
2019-09-14 23:08:42 +1200

    define castBool.

--- src/mustache.nim
+++ src/mustache.nim
@@ -91,26 +91,26 @@ type
   Context* = ref object
     values: Table[string, Value]
 
-proc asValue*(value: int): Value =
+proc castValue*(value: int): Value =
   Value(kind: vkInt, vInt: value)
 
-proc asValue*(value: float): Value =
+proc castValue*(value: float): Value =
   Value(kind: vkFloat, vFloat: value)
 
-proc asValue*(value: string): Value =
+proc castValue*(value: string): Value =
   Value(kind: vkString, vString: value)
 
-proc asValue*(value: bool): Value =
+proc castValue*(value: bool): Value =
   Value(kind: vkBool, vBool: value)
 
-proc asValue*[T](value: Table[string, T]): Value =
+proc castValue*[T](value: Table[string, T]): Value =
   let newValue = new(Table[string, Value])
   for k, v in value.pairs:
-    newValue[k] = v.asValue
+    newValue[k] = v.castValue
   Value(kind: vkTable, vTable: newValue)
 
-proc asValue*[T](value: seq[T]): Value =
-  Value(kind: vkSeq, vSeq: value.map(asValue))
+proc castValue*[T](value: seq[T]): Value =
+  Value(kind: vkSeq, vSeq: value.map(castValue))
 
 proc `[]=`*[T](ctx: Context, key: string, value: T) =
   ## Combine the key with a value within the given ctx.
@@ -119,7 +119,23 @@ proc `[]=`*[T](ctx: Context, key: string, value: T) =
   ##   ctx["name"] = 1
   ##   ctx["name"] = "string"
   ##   ctx["name"] = @[1,2,3]
-  ctx.values[key] = value.asValue
+  ctx.values[key] = value.castValue
+
+proc `[]`*(ctx: Context, key: string): Value =
+  if ctx.values.contains(key):
+    result = ctx.values[key]
+  else:
+    result = castValue("")
+
+proc castBool*(value: Value): bool =
+  case value.kind
+  of vkInt: value.vInt != 0
+  of vkFloat: value.vFloat != 0.0
+  of vkString: value.vString != ""
+  of vkBool: value.vBool
+  of vkSeq: value.vSeq.len != 0
+  of vkTable: value.vTable.len != 0
+  else: true
 
 when isMainModule:
   let ctx = Context(values: initTable[string,Value]())
